c0f8c1e120581c0366ed7fc25ece655df0636214
Leonardo Gaspar <leogaspar007@hotmail.com>
2023-07-18 17:26:15 -0300

    Changed the views.py file.

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
-from rest_framework.authentication import BasicAuthentication
-
+from rest_framework.response import Response
+from rest_framework import status
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -19,6 +19,15 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() +id
+            return response
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
